e8d6c8daa600a2ad275bf618d47849eb3e7acf64
insastrasbge1 <insastrasbge1@gmail.com>
2021-02-03 10:39:11 +0100

    fin C1

---
+++ src/main/java/fr/insa/beuvron/cours/m2/pasapaslb/dessin/Point.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insa.beuvron.cours.m2.pasapaslb.dessin;
+
+/**
+ *
+ * @author francois
+ */
+public class Point {
+    
+    private double px;
+    private double py;
+    
+    public double distance(Point p2) {
+        double dx = this.px - p2.px;
+        double dy = this.py - p2.py;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+    
+    public Point milieu(Point p2) {
+        Point res;
+        res = new Point();
+        res.px = (this.px + p2.px) /2;
+        res.py = (this.py + p2.py) /2;
+        return res;
+    }
+    
+    public static void testDistance() {
+        Point p1,point2,p3;
+        p1 = new Point();
+        p1.px = 0;
+        p1.py = 0;
+        point2 = new Point();
+        point2.px = 1;
+        point2.py = 1;
+        double dist = p1.distance(point2);
+        System.out.println("distance : " + dist);
+        p3 = p1.milieu(point2);
+        System.out.println("milieu : " + p3);
+    }
+    
+    public String toString() {
+        return "(" + this.px + "," + this.py + ")";
+    }
+    
+    public static void main(String[] args) {
+        testDistance();
+    }
+    
+}
