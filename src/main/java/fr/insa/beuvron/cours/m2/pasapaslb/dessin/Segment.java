8d0759158b0016c1644c5f174af7eee1af8deb15
insastrasbge1 <insastrasbge1@gmail.com>
2021-02-03 10:42:13 +0100

    fin2 C1

---
+++ src/main/java/fr/insa/beuvron/cours/m2/pasapaslb/dessin/Segment.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insa.beuvron.cours.m2.pasapaslb.dessin;
+
+/**
+ *
+ * @author francois
+ */
+public class Segment {
+    
+    private Point debut;
+    private Point fin;
+    
+    public static void main(String[] args) {
+        System.out.println("je suis dans segment 2");
+    }
+        
+}
