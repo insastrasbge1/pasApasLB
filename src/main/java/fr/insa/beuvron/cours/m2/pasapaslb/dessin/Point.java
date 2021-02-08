1161fa672d4ea80fbb54f653bea09dafe72cae2e
insastrasbge1 <insastrasbge1@gmail.com>
2021-02-08 15:56:48 +0100

    fin C2

--- src/main/java/fr/insa/beuvron/cours/m2/pasapaslb/dessin/Point.java
+++ src/main/java/fr/insa/beuvron/cours/m2/pasapaslb/dessin/Point.java
@@ -10,44 +10,65 @@ package fr.insa.beuvron.cours.m2.pasapaslb.dessin;
  * @author francois
  */
 public class Point {
-    
+
     private double px;
     private double py;
-    
+
+    public Point() {
+        this(0.0, 0.0);
+    }
+
+    public Point(double abs, double ord) {
+        this.px = abs;
+        this.py = ord;
+    }
+
+    public double getPx() {
+        return this.px;
+    }
+
+    public void setPx(double px) {
+        this.px = px;
+    }
+
+    public double getPy() {
+        return this.py;
+    }
+
+    public void setPy(double px) {
+        this.px = py;
+    }
+
     public double distance(Point p2) {
         double dx = this.px - p2.px;
         double dy = this.py - p2.py;
-        return Math.sqrt(dx*dx + dy*dy);
+        return Math.sqrt(dx * dx + dy * dy);
     }
-    
+
     public Point milieu(Point p2) {
         Point res;
         res = new Point();
-        res.px = (this.px + p2.px) /2;
-        res.py = (this.py + p2.py) /2;
+        res.px = (this.px + p2.px) / 2;
+        res.py = (this.py + p2.py) / 2;
         return res;
     }
-    
+
     public static void testDistance() {
-        Point p1,point2,p3;
-        p1 = new Point();
-        p1.px = 0;
-        p1.py = 0;
-        point2 = new Point();
-        point2.px = 1;
-        point2.py = 1;
+        Point p1, point2, p3;
+        p1 = new Point(0, 0);
+        point2 = new Point(1, 1);
         double dist = p1.distance(point2);
         System.out.println("distance : " + dist);
         p3 = p1.milieu(point2);
         System.out.println("milieu : " + p3);
     }
-    
+
     public String toString() {
         return "(" + this.px + "," + this.py + ")";
     }
-    
+
     public static void main(String[] args) {
         testDistance();
     }
-    
+
 }
--- src/main/java/fr/insa/beuvron/cours/m2/pasapaslb/dessin/Segment.java
+++ src/main/java/fr/insa/beuvron/cours/m2/pasapaslb/dessin/Segment.java
@@ -14,8 +14,48 @@ public class Segment {
     private Point debut;
     private Point fin;
     
+    public Segment(Point debut,Point fin) {
+        this.debut = debut;
+        this.fin = fin;
+    }
+    
+     /**
+     * @return the debut
+     */
+    public Point getDebut() {
+        return debut;
+    }
+
+    /**
+     * @return the fin
+     */
+    public Point getFin() {
+        return fin;
+    }
+     
+    
+    public String toString() {
+        return "[" + this.debut +
+                " , " + this.fin + "]" ;
+        
+    }
+    
+    public double longueur() {
+        return this.debut.distance(this.fin);
+    }
+    
     public static void main(String[] args) {
         System.out.println("je suis dans segment 2");
-    }
+        Segment s1 ;
+        Point f;
+        f = new Point(5,6);
         
+        //..
+        double ab = f.getPx();
+        f.setPx(12);
+        s1 = new Segment(new Point(3,4),f);
+        System.out.println("s1 = " + s1);
+    }
+
+      
 }
